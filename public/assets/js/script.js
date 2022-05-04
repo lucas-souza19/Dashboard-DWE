406ba24892408cffb25ddc0fd8225091c6d3aa5b
Lucas Santos Souza <lucassouzacps19@gmail.com>
2022-05-04 17:22:07 -0300

    Atualizações Dinâmicas

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const aleteraTema = document.querySelector(".alterar-tema");
+var arr_pedidos = [];
 
 // EXIBIR MENU
 menuBtn.addEventListener('click', () => {
@@ -33,34 +34,51 @@ document.addEventListener('DOMContentLoaded', function (evento) {
     }
 });
 
-// INSERIR PEDIDOS NA TABELA
-(function pedidosJson() {
-    let url = "https://dashboard-dwe2-default-rtdb.firebaseio.com/Pedidos.json";
+document.addEventListener("DOMContentLoaded", function(event) {
 
-    fetch(url).then(response => response.json())
-        .then(pedidos => {
-            createTable(pedidos);
-        });
-})();
+    // INSERIR PEDIDOS NA TABELA
+    (function pedidosJson() {
+        let url = "https://dashboard-dwe2-default-rtdb.firebaseio.com/Pedidos.json";
+
+        fetch(url).then(response => response.json())
+            .then(pedidos => {
+                createTable(pedidos);
+            });
+    })();
+
+    // INSERIR PESSOAS NA TABELA
+    (function pessoasJson() {
+        let url2 = "https://dashboard-dwe2-default-rtdb.firebaseio.com/Pessoas.json";
+
+        fetch(url2).then(response => response.json())
+            .then(pessoas => {
+                createAtt(pessoas, arr_pedidos);
+            });
+
+    })();
+})
+
+
 
 function createTable(pedidos) {
-    
-    pedidos.forEach(pedidos => {
+
+    for (let i = 0; i < pedidos.length; i++) {
         let tr = document.createElement('tr');
 
         let td1 = document.createElement('td');
-        td1.textContent = pedidos.nomeProduto;
+        td1.textContent = pedidos[i].nomeProduto;
+        arr_pedidos[i] = pedidos[i].nomeProduto;
 
         tr.append(td1);
 
         let td2 = document.createElement('td');
-        td2.textContent = pedidos.statusPagamento;
+        td2.textContent = pedidos[i].statusPagamento;
 
         tr.append(td2);
 
         let td3 = document.createElement('td');
-        td3.textContent = pedidos.envio;
-        td3.classList = pedidos.envio === 'Devolvido' ? 'erro' : pedidos.envio === 'Pendente' ? 'alerta' : pedidos.envio === 'Enviado' ? 'sucesso' : 'primaria'
+        td3.textContent = pedidos[i].envio;
+        td3.classList = pedidos[i].envio === 'Devolvido' ? 'erro' : pedidos[i].envio === 'Pendente' ? 'alerta' : pedidos[i].envio === 'Enviado' ? 'sucesso' : 'primaria'
 
         tr.append(td3);
 
@@ -71,7 +89,43 @@ function createTable(pedidos) {
         tr.append(td4);
 
         document.querySelector('table tbody').appendChild(tr);
-    });
+    };
 }
 
-pedidosJson();
+
+function createAtt(pessoas, pedidos) {
+
+    for (let i = 0; i < pessoas.length; i++) {
+        debugger
+        let div1 = document.createElement('div');
+        div1.classList.add("atualizacao");
+
+        let div2 = document.createElement('div');
+        div2.classList.add("foto-perfil");
+
+        let img = document.createElement('img');
+        img.setAttribute('src', './assets/images/if_icon.png');
+        div2.append(img);
+
+        div1.append(div2);
+
+        let div3 = document.createElement('div');
+
+        let p = document.createElement('p');
+        p.textContent = pessoas[i].nome + " recebeu o pedido de " + pedidos[i];
+        div3.append(p);
+
+        let small = document.createElement('small');
+        small.classList.add('legenda');
+        small.textContent = i + " Minutos Atrás;"
+        div3.append(small);
+
+        div1.append(div3);
+
+        document.getElementById('att').appendChild(div1);
+
+        if (i > 2) {    
+            break;
+        }
+    }
+}
